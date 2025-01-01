ba8ced0ffcbf0761ceecfff79a0862708b0da84b
rweil55 <github@shaw-weil.com>
2025-01-01 07:58:06 -0500

    added copyright and license

--- planet_walk.js
+++ planet_walk.js
@@ -1,3 +1,10 @@
+
+/*  Copyright by ROy Weil 2024
+ *  Planet walk calculations by Roy Weil is licensed under CC BY-SA 4.0
+ *      Attribution Required, Share Alike
+*       https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1
+*   must include the trailer information on any related html page.
+*/
 var planetNames = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'];
 var astroInner = [0, 36, 67, 93, 141, 483, 886, 1782, 2793, 3670];
 var astroOuter = [0, 46, 72, 100, 152, 520, 952, 1920, 3000, 3950]; // in million miles
@@ -15,8 +22,8 @@ function displayMenu() {
         '</li>' +
         '</ul>';
     trailerLocation = document.getElementById('trailer');
-    trailerLocation.innerHTML = 'This page brought to you via the regional bicycle guide book <a href="https://freewheelingeasy.com" >FreewheelingEasy in Western Pennsylvania</a></p>' +
-        '<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://planetwalk.freewheelingeasy.com/">Planet walk calculations</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://royweil.com/">Roy Weil</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>.</p>';
+    trailerLocation.innerHTML = 'This page brought to you via the regional bicycle guide book <a href="https://freewheelingeasy.com" >FreewheelingEasy in Western Pennsylvania</a><br>' +
+        '<span xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://planetwalk.freewheelingeasy.com/">Planet walk calculations</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://royweil.com/">Roy Weil</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>.</span>';
 
     /*
        ' <div id="sub" class="submenu"> ' +
